4529e1865bdc82a9a5f22dce1544732ba0dffda4
Nuno Farinha <nadfarinha@gmail.com>
2019-02-06 22:34:49 +0000

    Change student seed data to use the correct house id value

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,9 @@
 require_relative('../models/house')
 require_relative('../models/student')
 
-student1 = Student.new({
-  "first_name" => "Harry",
-  "last_name" => "Potter",
-  "age" => 20,
-  "house_id" => 1
-  })
+Student.delete_all()
+House.delete_all()
 
-student2 = Student.new({
-    "first_name" => "Neville",
-    "last_name" => "Longbottom",
-    "age" => 20,
-    "house_id" => 1
-    })
 
 house1 = House.new({
   "name" => "Gryffindor",
@@ -25,8 +15,33 @@ house2 = House.new({
   "url" => "https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Slytherin_colours.svg/200px-Slytherin_colours.svg.png"
   })
 
-house1.save
-house2.save
+
+
+  house1.save
+  house2.save
+
+student1 = Student.new({
+  "first_name" => "Harry",
+  "last_name" => "Potter",
+  "age" => 20,
+  "house_id" => house1.id
+  })
+
+student2 = Student.new({
+    "first_name" => "Neville",
+    "last_name" => "Longbottom",
+    "age" => 20,
+    "house_id" => house1.id
+    })
+
+student3 = Student.new({
+    "first_name" => "Draco",
+    "last_name" => "Malfoy",
+    "age" => 22,
+    "house_id" => house2.id
+    })
+
 
 student1.save
 student2.save
+student3.save

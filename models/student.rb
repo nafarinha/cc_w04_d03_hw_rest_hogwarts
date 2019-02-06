fd2fa9ab3125cf9be20fe817ad8d529378a49986
Nuno Farinha <nadfarinha@gmail.com>
2019-02-06 22:41:08 +0000

    Add house method to student class

--- models/student.rb
+++ models/student.rb
@@ -5,10 +5,11 @@ require('pry')
 
 class Student
 
-attr_reader :id, :first_name, :last_name, :age, :house_id
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :age, :house_id
 
   def initialize(options)
-    @id = options["id"].to_i
+    @id = options["id"].to_i if options["id"]
     @first_name = options['first_name']
     @last_name = options['last_name']
     @age = options['age'].to_i
@@ -17,8 +18,18 @@ attr_reader :id, :first_name, :last_name, :age, :house_id
 
   def pretty_name
     return "#{@first_name} #{@last_name}"
-  end 
+  end
 
+  def house()
+    sql = "SELECT h.* FROM houses h
+      INNER JOIN students s
+        ON s.house_id = h.id
+      WHERE s.id = $1"
+    values = [@id]
+    house = SqlRunner.run(sql, values)
+    return house.first()
+
+  end
 
   def save()
     sql = "INSERT INTO students
@@ -32,26 +43,33 @@ attr_reader :id, :first_name, :last_name, :age, :house_id
     (
       $1, $2, $3, $4
     )
-    RETURNING *"
+    RETURNING id"
     values = [@first_name, @last_name, @age, @house_id]
     student = SqlRunner.run(sql, values)
     @id = student.first()['id'].to_i
   end
 
   def self.all()
-  sql = "SELECT * FROM students"
-  students = SqlRunner.run( sql )
-  result = students.map { |student| Student.new( student) }
-  return result
-end
+    sql = "SELECT * FROM students"
+    students = SqlRunner.run( sql )
+    result = students.map { |student| Student.new( student) }
+    return result
+  end
 
-def self.find( id )
-  sql = "SELECT * FROM students WHERE id = $1"
-  values = [id]
-  student = SqlRunner.run( sql, values )
-  result = Student.new( student.first )
-  return result
-end
+  def self.find( id )
+    sql = "SELECT * FROM students WHERE id = $1"
+    values = [id]
+    student = SqlRunner.run( sql, values )
+    result = Student.new( student.first )
+    return result
+  end
+
+  def self.delete_all()
+   sql = "DELETE FROM students"
+   SqlRunner.run(sql)
+ end
+
+
 
 
 end
